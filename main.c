62218dfd13e572a16d82d0d786a97e693c851b2d
Psyche <pearlbear@hotmail.com>
2014-11-09 19:43:53 +0800

    用于测试算法时间

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ main(){
   int presentTime;
   presentTime = clock();
   
-
+  
 
   presentTime = clock() - presentTime;
   printf("It took %ds ticks (%f seconds).\n", presentTime, ((double)presentTime)/CLOCKS_PER_SEC);
