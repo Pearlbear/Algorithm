ea0a7c24af2793b2202e576177af2ed2410b67d5
Psyche <pearlbear@hotmail.com>
2014-11-09 19:15:23 +0800

    初始化

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <time.h>//clock()
+main(){
+  int presentTime;
+  presentTime = clock();
+  
+
+
+  presentTime = clock() - presentTime;
+  printf("It took %ds ticks (%f seconds).\n", presentTime, ((double)presentTime)/CLOCKS_PER_SEC);
+}
