0176c8cdcb92342497df60f27bef62e86a63d1fc
Psyche <pearlbear@hotmail.com>
2015-02-28 21:37:54 +0800

    update list.c

--- DataStructure/LinkedList/list.c
+++ DataStructure/LinkedList/list.c
@@ -35,11 +35,47 @@ Position Find(List L, ElementType X)
 
 void Delete(List L, ElementType X)
 {
-  
+  Postion P, TmpCell;
+  P = FindPrevious(L, X);
+  if(!IsLast(P, L))
+    {
+      TmpCell = P->Next;
+      P->Next = TmpCell->Next;
+      free(TmpCell);
+      TmpCell = NULL;//是否加上这句会好些？还是在画蛇添足？把TmpCell里面存储的地址也删除掉？还是会自动删除
+    }
 }
 
 Position FindPrevious(List L, ElementType X)
 {
   Position P;
-  P = L->Next;
+  P = L;
+  while(P->Next != NULL && P->Next->Element != X)
+    P = P->Next;
+  return P;
+}
+
+void Insert(ElementType X, List L, Position P)
+{
+  Position TmpCell;
+  TmpCell = malloc(sizeof(struct Node));
+  if(TmpCell == NULL)
+    FatalError("未分配空间");
+  TmpCell->Element = X;
+  TmpCell->Next = P->Next;
+  P->Next = TmpCell;
+}
+/*
+  书上的MakeEmpty和DeleteList不是在做同一件事吗？MakeEmpty调用了DeleteList，此时L->Next已经指向NULL，这时又重新开辟空间，重新让L->Next指向NULL？有何意义？
+ */
+void DeleteList(List L)
+{
+  Position P, TmpCell;
+  P = L;
+  while(P!=Null)
+    {
+      TmpCell = P->Next;
+      free(P);
+      P = TmpCell;
+    }
 }
