4c0d7fc11316c48135d74f5205d481b476900dc2
Psyche <pearlbear@hotmail.com>
2015-04-02 00:24:12 +0800

    Update Q1

--- ProjectEuler/Q1.c
+++ ProjectEuler/Q1.c
@@ -1,3 +1,8 @@
+/*
+ * @author: Psyche
+ * time complexity: O(N)
+ */
+/*
 int SumOfMultipleOfThreeAndFive(int limit)
 {
   int i,sum;
@@ -6,4 +11,19 @@ int SumOfMultipleOfThreeAndFive(int limit)
       if(i%3 == 0 || i%5 == 0)
 	sum += i;
   return sum;
+}*/
+
+/*
+ * @author: Euler
+ * time complexity: O(1)
+ */
+static int SumOfDivisibleBy(int divisor, int limit)
+{
+  int n = (limit - 1) / divisor;
+  return n * (1 + n)/2 * divisor;
 }
+int SumOfMultipleOfThreeAndFive(int limit)
+{
+  return SumOfDivisibleBy(3, limit) + SumOfDivisibleBy(5, limit) - SumOfDivisibleBy(15, limit);
+}
+
