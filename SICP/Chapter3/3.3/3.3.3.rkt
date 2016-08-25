c10d8069c333a0efa875a18cae50f03d0b49455b
psychesworld <psychesworld@gmail.com>
2016-08-25 22:54:36 +0800

    3.3.3

---
+++ SICP/Chapter3/3.3/3.3.3.rkt
@@ -0,0 +1,32 @@
+#lang planet neil/sicp
+(define (make-table)
+  (let ((local-table (list '*table*)))
+    (define (lookup key-1 key-2)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (cdr record)
+                  false))
+            false)))
+    (define (insert! key-1 key-2 value)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (set-cdr! record value)
+                  (set-cdr! subtable
+                            (cons (cons key-2 value)
+                                  (cdr subtable)))))
+            (set-cdr! local-table
+                      (cons (list key-1
+                                  (cons key-2 value))
+                            (cdr local-table))))))
+    (define (dispatch m)
+      (cond ((eq? m 'lookup) lookup)
+            ((eq? m 'insert!) insert!)
+            (else (error "Unsupported operation!"))))
+    dispatch))
+(define operation-table (make-table))
+(define get (operation-table 'lookup))
+(define put (operation-table 'insert!))
