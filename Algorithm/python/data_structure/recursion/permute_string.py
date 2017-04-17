25cfe1ce2809e7b1f096a80a2b1396e209bc3114
psychesworld <psychesworld@gmail.com>
2017-04-17 22:35:41 +0800

    Permute all possible strings in repeated string

--- Algorithm/python/data_structure/recursion/permute_string.py
+++ Algorithm/python/data_structure/recursion/permute_string.py
@@ -3,8 +3,15 @@ from unittest import TestCase
 
 class TestPermuteString(TestCase):
     def test_permute_string(self):
-        permutations = permute_string('abc')
-        self.assertEqual({'abc', 'acb', 'bac', 'bca', 'cab', 'cba'}, set(permutations))
+        ab_permutations = permute_string('ab')
+        self.assertEqual(len(ab_permutations), 2)
+        self.assertEqual({'ab', 'ba'}, set(ab_permutations))
+        abc_permutations = permute_string('abc')
+        self.assertEqual(len(abc_permutations), 6)
+        self.assertEqual({'abc', 'acb', 'bac', 'bca', 'cab', 'cba'}, set(abc_permutations))
+        aac_permutations = permute_string('aac')
+        self.assertEqual(len(aac_permutations), 3)
+        self.assertEqual({'aac', 'aca', 'caa'}, set(aac_permutations))
 
 
 def permute_string(string):
@@ -15,16 +22,23 @@ def permute_string(string):
         swap_string[index1], swap_string[index2] = swap_string[index2], swap_string[index1]
         return ''.join(swap_string)
 
+    def is_swap(replace_string, index1, index2):
+        if index1 == index2:
+            return True
+        first_to_last_second, last = replace_string[0: index2], replace_string[index2]
+        for j in range(0, index2):
+            if first_to_last_second[j] == last:
+                return False
+        return True
+
     if not string:
         return []
     if len(string) == 1:
         return [string]
-    if len(string) == 2:
-        return [string, swap(string, 0, 1)]
     result = []
     for i in range(0, len(string)):
-        replaced = swap(string, 0, i)
-        first, rest = replaced[0], replaced[1:]
-        result += list(map(lambda item: first + item, permute_string(rest)))
+        if is_swap(string, 0, i):
+            replaced = swap(string, 0, i)
+            first, rest = replaced[0], replaced[1:]
+            result += list(map(lambda item: first + item, permute_string(rest)))
     return result
-
